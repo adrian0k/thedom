c2eb1d7d2085af171e4a11cc94ec5e9a8130d267
adrian0k <ismd1@e303-s07.lab.lagcc>
2016-06-08 21:12:10 -0400

    PhotosNextandBefore

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 // alert("hello world");
+var NroImagen = 1;
+var NombreImagen = "images/classProject-";
+var ImagenPrincipal = document.getElementById("main_image");
+
+var nextButton = document.getElementById("next");
+var prevButton = document.getElementById("prev");
+
+
+
+console.log (ImagenPrincipal.src, nextButton, prevButton);
+
+// on click
+function Siguiente()
+{
+if (NroImagen<5)
+{
+  NroImagen = NroImagen + 1;
+}
+else {
+  NroImagen=5
+}
+var NuevoSrc = NombreImagen + NroImagen + ".jpg";
+//document.write(NuevoSrc);
+console.log(NuevoSrc);
+ImagenPrincipal.src=NuevoSrc;
+}
+
+
+function Anterior()
+{
+  if (NroImagen>=2)
+  {
+    NroImagen = NroImagen - 1;
+  }
+  else {
+    NroImagen = 1;
+  }
+    var NuevoSrc = NombreImagen + NroImagen + ".jpg";
+    //document.write(NuevoSrc);
+    console.log(NuevoSrc);
+    ImagenPrincipal.src=NuevoSrc;
+}
+
+nextButton.onclick = Siguiente;
+prevButton.onclick = Anterior;
